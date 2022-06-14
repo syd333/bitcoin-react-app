aea4ed6fcfc0ef89075f1f13259cf91de546b0d4
syd333 <sydmiranda@icloud.com>
2022-06-14 14:31:23 -0400

    data coming through

--- bitcoin-app/src/App.js
+++ bitcoin-app/src/App.js
@@ -1,10 +1,31 @@
+/* eslint-disable no-console */
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [players, getPlayersData] = useState({});
+
+  const fetchData = async () => {
+    const { data } = await axios.get('https://api.sleeper.app/v1/players/nfl');
+    getPlayersData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log(players);
+
+  const arrayOfPlayers = Object.entries(players);
+  console.log(arrayOfPlayers.slice(0, 4));
   return (
     <div className="App">
-      <header className="App-header">bitcoin react app</header>
+      {arrayOfPlayers.slice(0, 9).map((player) => (
+        <ul>
+          <li>{`${player[1].first_name} ${player[1].last_name}`}</li>
+        </ul>
+      ))}
     </div>
   );
 }
---
+++ bitcoin-app/src/PlayerTable.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+
+function PlayerTable() {
+  return <div>table</div>;
+}
+
+export default PlayerTable;
